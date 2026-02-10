b38f9f1505a021d2cbd6119469af0eaf6d9b290b
Jepoy <poyhidalgo@gmail.com>
2026-02-10 12:48:31 +0800

    chore: add neondb connection and configuration

--- server/main.go
+++ server/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Polqt/ocealis/db"
+)
+
 func main() {
-	
+	if err := db.Connect(); err != nil {
+		log.Fatalf("DB connection error: %v", err)
+	}
+
+	defer db.Close()
+
+	fmt.Println("Server starting...")
+
 }
