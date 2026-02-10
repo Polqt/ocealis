2513135e82313f7073a096033408e14fa4077bba
Jepoy <poyhidalgo@gmail.com>
2026-02-10 16:45:33 +0800

    chore: add with fiber initialization and middleware

--- server/main.go
+++ server/main.go
@@ -4,12 +4,19 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Polqt/ocealis/db"
 	"github.com/Polqt/ocealis/db/ocealis"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	if err := db.Connect(); err != nil {
 		log.Fatalf("DB connection error: %v", err)
 	}
@@ -25,4 +32,22 @@ func main() {
 	}
 	fmt.Printf("Users in database: %v\n", users)
 
+	// Initialize fiber
+	app := fiber.New(fiber.Config{
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Ocealis",
+		AppName:       "Ocealis v1",
+	})
+
+
+	// Middleware
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{os.Getenv("CORS_ALLOWED_ORIGINS")},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
+
+	// Routes
 }
